a9526da26878596debafd78fc2d517b10aa1f43d
Chris Hitchcott <hitchcott@gmail.com>
2017-03-13 11:26:35 +0800

    [feature] initiallyOpen; child passed setError, setLoading

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import { Icon, Message, Modal, Button, Form, Dimmer, Loader } from 'semantic-ui-
 export default class EZModal extends Component {
   constructor(props) {
     super(props);
-    this.state = { showing: false, formData: this.props.data || {} };
+    this.state = { showing: this.props.initiallyOpen, error: null, loading: false, formData: this.props.data || {} };
     this.handleShowToggle = this.handleShowToggle.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleThisSubmit = this.handleThisSubmit.bind(this);
     this.handleResetFormData = this.handleResetFormData.bind(this);
     this.handleSetFormData = this.handleSetFormData.bind(this);
+    this.handleSetError = this.handleSetError.bind(this);
+    this.handleSetLoading = this.handleSetLoading.bind(this);
   }
   handlePromiseOrFunc(promiseOrFunc) {
     if (promiseOrFunc && typeof promiseOrFunc.then === 'function') {
@@ -19,18 +21,26 @@ export default class EZModal extends Component {
   }
   handleShowToggle(showing) {
     const update = { showing };
-    if (showing) { update.formData = this.props.data || {}; }
-    this.setState(update);
+    if (showing) {
+      update.formData = this.props.data || {};
+    }
+    this.setState({ ...update, error: null, loading: false });
   }
   handleSetFormData(data) {
     this.setState({ formData: data });
   }
   handleResetFormData() {
-    this.setState({ formData: this.props.data });
+    this.setState({ formData: this.props.data, error: null, loading: false });
     if (this.props.onReset) { this.props.onReset(); }
   }
   handleChange(e) {
     this.setState({ formData: { ...this.state.formData, [e.target.name]: e.target.value } });
+  }
+  handleSetError(error, errorHeader) {
+    this.setState({ error, errorHeader: error && errorHeader });
+  }
+  handleSetLoading(loading) {
+    this.setState({ loading });
   }
   handleThisSubmit(e) {
     e.preventDefault();
@@ -58,12 +68,20 @@ export default class EZModal extends Component {
       noCloseButton, noSubmitButton,
       closeButtonText, submitButtonText,
       /* eslint-disable no-unused-vars */
-      handleSubmit, loading, error, errorHeader, // plucked so we can pass modalProps
+      handleSubmit, loading, error, errorHeader, initiallyOpen, // plucked so we can pass otherProps
       /* eslint-enable no-unused-vars */
-      ...modalProps,
+      ...otherProps,
     } = this.props;
     const { showing, formData } = this.state;
-    const { handleSetFormData, handleChange, handleThisSubmit, handleShowToggle, handleResetFormData } = this;
+    const {
+      handleSetError,
+      handleSetFormData,
+      handleChange,
+      handleThisSubmit,
+      handleShowToggle,
+      handleResetFormData,
+      handleSetLoading,
+    } = this;
     const hide = () => { if (onClose) { onClose(); } handleShowToggle(false); };
     const childProps = {
       data,
@@ -73,6 +91,8 @@ export default class EZModal extends Component {
       resetFormData: handleResetFormData,
       change: handleChange,
       submit: handleThisSubmit,
+      setError: handleSetError,
+      setLoading: handleSetLoading,
     };
     const notShowingClosed = typeof noCloseButton === 'function' ? noCloseButton(childProps) : noCloseButton;
     const notShowingSubmit = typeof noSubmitButton === 'function' ? noSubmitButton(childProps) : noSubmitButton;
@@ -82,33 +102,36 @@ export default class EZModal extends Component {
       !notShowingClosed && <Button key="close" content={closeButtonContent || 'Cancel'} onClick={hide} />,
       !notShowingSubmit && <Button type="submit" key="submit" positive icon="checkmark" labelPosition="right" content={submitButtonContent || 'OK'} onClick={handleThisSubmit} />,
     ];
-    const loaderContent = typeof loading === 'boolean' ? undefined : loading;
+    const activeLoading = loading || this.state.loading;
+    const loaderContent = typeof activeLoading === 'boolean' ? undefined : activeLoading;
+    const activeError = error || this.state.error;
+    const activeErrorHeader = errorHeader || this.state.errorHeader;
     return (
       <Modal
-        {...modalProps}
+        {...otherProps}
         trigger={trigger}
         open={showing}
         onOpen={() => handleShowToggle(true)}
         onClose={hide}
       >
-        {loading && <Dimmer active inverted><Loader content={loaderContent} /></Dimmer>}
+        {activeLoading && <Dimmer active inverted><Loader content={loaderContent} /></Dimmer>}
         {header && <Modal.Header>{header}</Modal.Header>}
         <Modal.Content>
           {this.props.handleSubmit ? // only use a form if we expect a submit
-            <Form onSubmit={handleThisSubmit} error={!!error}>
+            <Form onSubmit={handleThisSubmit}>
               {this.renderCompOrFunc(content, childProps)}
-              {error &&
-                <Message icon error>
-                  <Icon name="warning sign" />
-                  <Message.Content>
-                    <Message.Header>{errorHeader || 'Oops, something went wrong'}</Message.Header>
-                    {`${error}`}
-                  </Message.Content>
-                </Message>
-              }
             </Form>
           :
             this.renderCompOrFunc(content, childProps)
+          }
+          {activeError &&
+            <Message icon error>
+              <Icon name="warning sign" />
+              <Message.Content>
+                <Message.Header>{activeErrorHeader || 'Oops, something went wrong'}</Message.Header>
+                {`${activeError}`}
+              </Message.Content>
+            </Message>
           }
         </Modal.Content>
         {actions !== false &&
@@ -145,10 +168,11 @@ export default class EZModal extends Component {
 }
 
 EZModal.propTypes = {
-  trigger: PropTypes.object.isRequired,
+  trigger: PropTypes.object,
   loading: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
   data: PropTypes.object,
   error: PropTypes.any,
+  initiallyOpen: PropTypes.bool,
   errorHeader: PropTypes.string,
   onClose: PropTypes.func,
   onReset: PropTypes.func,
